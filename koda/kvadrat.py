19f26e20620363fbc97a3e1db8691e1724d4b8a5
Tim Kalan <timkalan99@gmail.com>
2020-12-02 22:48:49 +0100

    dodal 3D grafe in funkcionalnost

--- koda/kvadrat.py
+++ koda/kvadrat.py
@@ -1,19 +1,25 @@
 """
 Hm.
 """
+# za 3D risanje
+from mpl_toolkits.mplot3d import Axes3D 
 
 import random
+# osnovna knjižnica za risanje
 import matplotlib.pyplot as plt
 
+# prednostna vrsta
+from queue import PriorityQueue
 
+
+# računanje evklidske razdalje, za vsak slučaj dela v R ** n
 def razdalja(tocka0, tocka1):
-    """
-    Izračuna evklidsko razdaljo med danima točkama.
-    """
-    # potencialno spremeni, da dela v R^n
-    dx = tocka0[0] - tocka1[0]
-    dy = tocka0[1] - tocka1[1]
-    return (dx**2 + dy**2) ** (1/2)
+    razdalja = 0
+    for i in range(len(tocka0)):
+        dx = tocka0[i] - tocka1[i]
+        razdalja += dx **2
+
+    return razdalja ** (1/2)
 
 
 class Kvadrat:
@@ -27,7 +33,7 @@ class Kvadrat:
         st_tock = število točk v enotskem kvadratu 
         """
         self.st_tock = st_tock
-        #self.max_razdalja = max_razdalja
+        self.max_razdalja = max_razdalja
         
         # generiramo vse potrebne točke, shranimo kot seznam naborov
         tocke = []
@@ -36,7 +42,6 @@ class Kvadrat:
             x, y = random.uniform(0, 1), random.uniform(0, 1)
             tocke.append((x, y))
         self.tocke = tocke
-        self.max_razdalja = max_razdalja
 
     
     def bliznje(self):
@@ -45,20 +50,22 @@ class Kvadrat:
         hkrati služi kot graf v obliki seznama sosedov.
         """
         bliznje = dict()
-        for (x, y) in self.tocke:
+        for koren in self.tocke:
             ustrezne = []   # seznam točk, ki so dovolj blizu
-            for (a, b) in self.tocke:
-                if (x, y) == (a, b):
+            for sosed in self.tocke:
+                if koren == sosed:
                     # v seznam ne dodamo točke same
                     pass
 
-                elif razdalja((x, y), (a, b)) <= self.max_razdalja:
-                    ustrezne.append((a, b))
+                elif razdalja(koren, sosed) <= self.max_razdalja:
+                    ustrezne.append(sosed)
             
-            bliznje[(x, y)] = ustrezne
-        #self.koreni = list(bliznje.keys())      # grdo, da je to stranski učinek 
+            bliznje[koren] = ustrezne
         return bliznje
 
+
+    def dosezena(self, koren=0):
+        pass
 
 
     def drevo_najkrajsih_poti(self, koren=0):
@@ -107,6 +114,56 @@ class Kvadrat:
                     oce[sosed] = u
 
         return oddaljenost, oce
+
+
+    def drevo_najkrajsih_poti_hitro(self, koren=0):
+        """
+        Uporabi Dijkstrov algoritem za izračun drevesa najkrajših poti
+        od dane točke. Izvajaje je pohitreno s pomočjo prednostne vrste.
+
+        koren = začetna točka, podana kot zaporedna številka (začnemo z 0)
+        """
+        # za vsako točko dobimo njene sosede
+        seznam_sosedov = self.bliznje()
+        
+        # koreni so vse točke ki so si dovolj blizu
+        koreni = list(seznam_sosedov.keys())
+
+        # pogledamo, če smo si izbrali ustrezen koren
+        try:
+            root = koreni[koren]
+            self.root = root         # zabeležimo, kaj je koren (za vizualizacijo)
+        except IndexError as e:
+            print("Premalo korenov!")
+            print(e)
+            
+        # dejanski začetek Dijkstrovega algoritma - še ni prednostne vrste
+        Q = PriorityQueue()
+        oddaljenost = dict()
+        oce = dict()
+
+        for vozlisce in seznam_sosedov:
+            if vozlisce != root:
+                oddaljenost[vozlisce] = 1000000        # praktično neskončno za naše namene
+                oce[vozlisce] = None
+
+            prioriteta = oddaljenost.get(vozlisce)
+            Q.put((prioriteta, vozlisce))
+
+        oddaljenost[root] = 0
+
+        while not Q.empty():
+            # določimo vozlišče z minimalno oddaljenostjo, ki je še v Q
+            u = Q.get()   # odstranimo iz seznama in vrnemo najboljšega
+
+            # pregledamo vse sosede opazovanega vozlišča, ki so še v Q
+            for sosed in [nei for nei in seznam_sosedov[u] if nei in Q]:
+                alt = oddaljenost[u] + razdalja(u, sosed)
+                if alt < oddaljenost[sosed]:
+                    oddaljenost[sosed] = alt
+                    oce[sosed] = u
+
+        return oddaljenost, oce
     
     
 ################################## VIZUALIZACIJA ########################################
@@ -133,14 +190,28 @@ class Kvadrat:
         # povedati moramo, katero točko smo si izbrali
         koren = list(bliznje.keys())[tocka]
         
-        # *zip(*seznam_parov) pomeni unzip: vrne dva seznama, in ju da kot dva argumenta
-        # za plt.scatter -> x in y
         plt.xlim(0, 1)
         plt.ylim(0, 1)
         try:
+            # *zip(*seznam_parov) pomeni unzip: vrne dva seznama, in ju da kot dva argumenta
+            # za plt.scatter -> x in y
             plt.scatter(*zip(*bliznje[koren]))
         except:
             print('Nimamo bližnjih točk!')
+
+
+    def narisi_okolico(self, tocka=0):
+        """
+        Nariše krožnico, ki predstavlja okolico dane točke, znotraj katere so točke, 
+        s katerimi se bo ta povezala.
+
+        tocka = zaporedno število točke v slovarju sosedov
+        """
+        # naša izbrana točka, v obliki (x, y)
+        koren = list(self.bliznje().keys())[tocka]
+
+        # narišemo krog s središčem v korenu in radijem max_razdalja
+        plt.gcf().gca().add_artist(plt.Circle(koren, self.max_razdalja, fill=False))
             
     
     def narisi_graf_tocke(self, tocka=0):
@@ -166,6 +237,13 @@ class Kvadrat:
         # sprehodimo se čez vse točke in uporabimo zgornjo funkcijo
         for i in range(len(self.tocke)):
             self.narisi_graf_tocke(i)
+
+    
+    def narisi_dosezena(self, koren=0):
+        """
+        Označi voslišča, ki smo jih dosegli iz našega vozlišča.
+        """
+        pass
             
             
     def narisi_drevo(self, koren=0):
@@ -187,8 +265,107 @@ class Kvadrat:
 
 
 
+class Kocka(Kvadrat):
+    """
+    Razred 'Kvadrat' preselimo v tri dimenzije.
+    """
+
+    def __init__(self, st_tock, max_razdalja, n_dim=3):
+        """
+        st_tock = število točk v enotskem kvadratu 
+        """
+        self.st_tock = st_tock
+        self.max_razdalja = max_razdalja
+        
+        # generiramo vse potrebne točke, shranimo kot seznam naborov
+        tocke = []
+        for _ in range(st_tock):
+            tocka = [random.uniform(0, 1) for _ in range(n_dim)]
+            tocke.append(tuple(tocka))
+        self.tocke = tocke
+
+    
+    def narisi_tocke(self):
+        """
+        Nariše naše generirane točke v enotskem kvadratu.
+        """
+        # *zip(*seznam_parov) pomeni unzip: vrne dva seznama, in ju da kot dva argumenta
+        # za plt.scatter -> x in y
+
+        fig = plt.figure()
+        ax = Axes3D(fig)
+
+        x, y, z = zip(*self.tocke)
+        ax.scatter(x, y, z)
+    
+
+    #TODO: ne dela for some reason
+    def narisi_bliznje(self, tocka=0):
+        """
+        Nariše vse dovolj bližnje točke dane točke.
+        
+        tocka = zaporedno število točke v slovarju sosedov
+        """
+        bliznje = self.bliznje()          # da ne računamo tega dvakrat
+        
+        # povedati moramo, katero točko smo si izbrali
+        koren = list(bliznje.keys())[tocka]
+
+        try:
+            # *zip(*seznam_parov) pomeni unzip: vrne dva seznama, in ju da kot dva argumenta
+            # za plt.scatter -> x in y
+            x, y, z = zip(*self.tocke)
+            Axes3D.scatter(x, y, z)
+        except:
+            print('Nimamo bližnjih točk!')
+
+
+    def narisi_graf_tocke(self, tocka=0):
+        """
+        Nariše graf, ki ga generira posamezna točka in njej dovolj bližnje točke.
+        """
+        # določimo bližnje točke in naš koren
+        bliznje = self.bliznje()
+        koren = list(bliznje.keys())[tocka]
+        
+        # pogledamo vse bližnje točke korena
+        for x, y, z in bliznje[koren]:
+            x0, x1 = koren[0], x
+            y0, y1 = koren[1], y
+            z0, z1 = koren[2], z
+            plt.plot([x0, x1], [y0, y1], [z0, z1], color='black')
+
+
+    def narisi_drevo(self, koren=0):
+        """
+        Nariše drevo najkrajših poti, ki se začne v izbranem korenu.
+        """
+        _, oce = self.drevo_najkrajsih_poti(koren)
+
+        # narišemo koren - zorder prisili, da je narisan kot vrhnji element grafa
+        plt.plot(self.root[0], self.root[1], self.root[2], marker='s', color='#FF0000', zorder=1000000)
+        
+        # pregledamo slovar očetov
+        for pred, nasl in oce.items():
+            # če ima 'sina', ga narišemo
+            if nasl != None:
+                x0, x1 = pred[0], nasl[0]
+                y0, y1 = pred[1], nasl[1]
+                z0, z1 = pred[2], nasl[2]
+                plt.plot([x0, x1], [y0, y1], [z0, z1], color='#23FF00')
+    
+
+demo3D = Kocka(100, 0.3)
+
+demo3D.narisi_tocke()
+demo3D.narisi_bliznje()
+
+plt.show()
+
 
 # TODO: dijkstra na bolj učinkovit način
 # TODO: razmisli, če lahko na učinkovit način implemetiraš bliznje
 # TODO: morda kakšne animacije, če se to da
 # TODO: 3D
+# TODO: da se nariše katera vozlišča so dosežena in katera ne - morda računa razmerje
+# TODO: oznake na osi
